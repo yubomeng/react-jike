7263f88dc6565e739f1d75a3393b4e2df89b310a
yubo <3105107535@qq.com>
2023-10-20 21:21:55 +0800

    first commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,51 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+const Login = () => {
+    const onFinish = (values) => {
+        console.log(values);
+    }
+
+
+    return (
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form onFinish={onFinish} validateTrigger="onBlur">
+                    <Form.Item
+                        name="mobile"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入手机号',
+                            },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '请输入正确的手机号格式'
+                            }
+                        ]}>
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item name="code"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入验证码',
+                            },
+                        ]}>
+                        <Input size="large" placeholder="请输入验证码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
+}
+
+export default Login
