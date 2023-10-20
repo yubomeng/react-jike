2f61bd7ebb1b2c3c2f119c14a9d664e1c9eb7545
yubo <3105107535@qq.com>
2023-10-20 22:29:20 +0800

    登录-使用redux维护token数据

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom'
 import router from './router';
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,21 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message } from 'antd'
 import logo from '@/assets/logo.png'
+import { useDispatch } from 'react-redux'
+import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-    const onFinish = (values) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const onFinish =  async (values) => {
         console.log(values);
+        // 触发异步action fetchLogin
+      await  dispatch(fetchLogin(values))
+        // 1.跳转到首页
+        navigate('/')
+        // 2.提示用户
+        message.success('登录成功')
     }
 
 
