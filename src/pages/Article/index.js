54d9789a8aff28738412bb8398c0c668644b39fe
yubo <3105107535@qq.com>
2023-10-21 20:00:55 +0800

    文章列表-携带id跳转编辑页

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
 import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Popconfirm } from 'antd'
 // 引入汉化包-时间选择器显示中文
 import locale from 'antd/es/date-picker/locale/zh_CN'
@@ -15,6 +15,7 @@ const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+    const navigate = useNavigate()
     const { channelList } = useChannel()
 
 
@@ -67,7 +68,7 @@ const Article = () => {
             render: data => {
                 return (
                     <Space size="middle">
-                        <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                        <Button type="primary" shape="circle" icon={<EditOutlined />} onClick={() => navigate(`/publish?id=${data.id}`) } />
                         {/* 气泡确认框 */}
                         <Popconfirm
                             title="删除文章"
