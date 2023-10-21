09e91fce8233af890a049f84cad473e0b331b938
yubo <3105107535@qq.com>
2023-10-21 11:42:24 +0800

    echarts 图标的渲染

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,36 @@
+import { useEffect, useRef } from 'react'
+import * as echarts from 'echarts'
+
 const Home = () => {
-    return <div>this is home</div>
+    const chartRef = useRef(null)
+    useEffect(() => {
+        // 1. 生成实例
+        const myChart = echarts.init(chartRef.current)
+        // 2. 准备图表参数
+        const option = {
+            xAxis: { 
+                type: 'category',
+                data: ['Vue', 'React', 'Angular']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [10,20,50],
+                    type: 'bar'
+                }
+            ]
+        }
+        // 3. 渲染参数
+        myChart.setOption(option)
+    }, [])
+
+    return (
+        <div>
+            <div ref={chartRef} style={{ width: '400px', height: '300px' }} />
+        </div >
+    )
 }
 
 export default Home
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { getToken } from './token'
+import { getToken, removeToken } from './token'
+import router from '@/router'
 
 const request = axios.create({
     baseURL: 'http://geek.itheima.net/v1_0',
@@ -26,6 +27,13 @@ request.interceptors.response.use((response) => {
 }, (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    // 监控401 token失效
+    console.dir(error)
+    if (error.response.status === 401) {
+        removeToken()
+        router.navigate('/login')
+        window.location.reload()
+    }
     return Promise.reject(error)
 })
 
