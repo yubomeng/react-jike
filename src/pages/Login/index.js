6ab45e52bbf963337a176ff37814a7b475ede288
yubo <3105107535@qq.com>
2023-10-21 10:47:48 +0800

    登录页面加载进度条实现

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,21 +1,33 @@
+/*
+ * @Author: yubo 3105107535@qq.com
+ * @Date: 2023-10-20 18:05:10
+ * @LastEditors: yubo 3105107535@qq.com
+ * @LastEditTime: 2023-10-21 10:42:40
+ * @FilePath: \react-jike\src\pages\Login\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import './index.scss'
 import { Card, Form, Input, Button, message } from 'antd'
 import logo from '@/assets/logo.png'
 import { useDispatch } from 'react-redux'
 import { fetchLogin } from '@/store/modules/user'
 import { useNavigate } from 'react-router-dom'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const Login = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const onFinish =  async (values) => {
+    const onFinish = async (values) => {
         console.log(values);
         // 触发异步action fetchLogin
-      await  dispatch(fetchLogin(values))
+        NProgress.start() // 页面加载进度条开始
+        await dispatch(fetchLogin(values))
         // 1.跳转到首页
         navigate('/')
         // 2.提示用户
         message.success('登录成功')
+        NProgress.done() // 页面加载进度条结束
     }
 
 
