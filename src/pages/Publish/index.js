9bbadba2b12a41327e68e88ff99e1fcd78358e97
yubo <3105107535@qq.com>
2023-10-21 15:57:36 +0800

    文章封面基础上传实现

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -52,6 +52,12 @@ const Publish = () => {
         createArticleAPI(reqData)
     }
 
+    // 上传回调
+    const [imageList, setImageList] = useState([])
+    const onChange = (value) => {
+        console.log('正在上传中', value);
+        setImageList(value.fileList)
+    }
 
     return (
         <div className="publish">
@@ -86,6 +92,30 @@ const Publish = () => {
                             {/* value属性 用户选中之后会自动收集起来作为接口的提交字段 */}
                             {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}   
                         </Select>
+                    </Form.Item>
+                    <Form.Item label="封面">
+                        <Form.Item name="type">
+                            <Radio.Group>
+                                <Radio value={1}>单图</Radio>
+                                <Radio value={3}>三图</Radio>
+                                <Radio value={0}>无图</Radio>
+                            </Radio.Group>
+                        </Form.Item>
+                        {/* 
+                            listType: 决定选择文件框的外观样式
+                            showUploadList:控制显示上传列表
+                         */}
+                        <Upload
+                            listType="picture-card"
+                            showUploadList
+                            action={'http://geek.itheima.net/v1_0/upload'}
+                            name="image"
+                            onChange={onChange}
+                        >
+                            <div style={{ marginTop: 8 }}>
+                                <PlusOutlined />
+                            </div>
+                        </Upload>
                     </Form.Item>
                     <Form.Item
                         label="内容"
