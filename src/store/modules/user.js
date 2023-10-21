1707d9bf0d8d668de53ffe4db2de22c94d5d2594
yubo <3105107535@qq.com>
2023-10-21 14:54:24 +0800

    封装 apis 模块

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,6 +2,7 @@
 import { removeToken, request } from "@/utils";
 import { createSlice } from "@reduxjs/toolkit";
 import { setToken as _setToken, getToken } from '@/utils'
+import { getProfileAPI, loginAPI } from "@/apis/user";
 const userStore = createSlice({
     name: 'user',
     // 数据状态
@@ -37,7 +38,7 @@ const userReducer = userStore.reducer
 const fetchLogin = (loginForm) => {
     return async (dispatch) => {
         // 1.发送异步请求
-        const res = await request.post('/authorizations', loginForm)
+        const res = await loginAPI(loginForm)
         // 2.提交同步action进行token的存入
         dispatch(setToken(res.data.token))
     }
@@ -46,7 +47,7 @@ const fetchLogin = (loginForm) => {
 // 获取个人用户信息异步方法
 const fetchUserInfo = () => {
     return async (dispatch) => {
-        const res = await request.get('/user/profile')
+        const res = await getProfileAPI()
         dispatch(setUserInfo(res.data))
     }
 }
