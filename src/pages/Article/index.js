2cd2112070a63edb2c4d9fdd6bc12c3260923bfe
yubo <3105107535@qq.com>
2023-10-21 19:16:59 +0800

    文章列表-筛选实现

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -94,20 +94,44 @@ const Article = () => {
             title: 'wkwebview离线化加载h5资源解决方案'
         }
     ]
-
+    // 筛选功能
+    // 1.准备参数
+    const [reqData, setReqData] = useState({
+        status: '',
+        channel_id: '',
+        begin_pubdate: '',
+        end_pubdate: '',
+        page: 1,    //当前页码 (默认为 1)
+        per_page: 5 // 当前页条数(默认为 5 )
+    })
 
     // 获取文章列表
     const [list, setList] = useState([])
     const [count, setCount] = useState(0)
     useEffect(() => {
         async function getList() {
-            const res = await getArticleListAPI()
+            const res = await getArticleListAPI(reqData)
             setList(res.data.results)
             setCount(res.data.total_count)
         }
         getList()
-    })
+    }, [reqData])
 
+    
+    // 2. 获取筛选数据
+    const onFinish = (formValue) => {
+        console.log(formValue);
+        // 3.把表单收集到的数据放到参数中(不可变的)
+        setReqData({
+            ...reqData,
+            channel_id: formValue.channel_id,
+            status: formValue.status,
+            begin_pubdate: formValue.date[0].format('YYYY-MM-DD'),
+            end_pubdate: formValue.date[1].format('YYYY-MM-DD')
+        })
+        // 4.重新拉取文章列表 + 渲染table逻辑重复的 --复用
+        // reqData依赖项发生变化，重复执行副作用函数
+    }
 
     return (
         <div>
@@ -120,7 +144,7 @@ const Article = () => {
                 }
                 style={{ marginBottom: 20 }}
             >
-                <Form initialValues={{ status: '' }}>
+                <Form initialValues={{ status: '' }} onFinish={onFinish}>
                     <Form.Item label="状态" name="status">
                         <Radio.Group>
                             <Radio value={''}>全部</Radio>
